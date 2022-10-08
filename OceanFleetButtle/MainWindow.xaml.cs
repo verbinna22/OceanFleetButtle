f5af7f3c40054ed89b831e00deb73310c5aa5dc6
В. Н. А. СП МКН <mylogininya@yandex.ru>
2022-10-08 18:48:46 +0300

    strange

--- OceanFleetButtle/MainWindow.xaml.cs
+++ OceanFleetButtle/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,12 +24,25 @@ namespace OceanFleetButtle
     {
         public MainWindow()
         {
-            DataGrid gridMy = new DataGrid();
-            int[] sheps = new int[10];
-            gridMy.ItemsSource = sheps;
+            var gridMy = new Grid();
+            gridMy.Background = Brushes.BlueViolet;
+            for (int i = 0; i < 10; i++)
+            {
+                var colDef1 = new ColumnDefinition();
+                gridMy.ColumnDefinitions.Add(colDef1);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                var colDef1 = new RowDefinition();
+                gridMy.RowDefinitions.Add(colDef1);
+            }
+            gridMy.Width = 500;
+            gridMy.Height = 500;
+            
+            gridMy.ShowGridLines = true;
 
-            canvy.Children.Add(gridMy);
             InitializeComponent();
+            canvy.Children.Add(gridMy);
         }
     }
 }
