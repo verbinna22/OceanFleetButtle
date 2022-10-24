69976f497d71000d9573565f60bdc388cdcd24a6
В. Н. А. СП МКН <mylogininya@yandex.ru>
2022-10-24 23:50:40 +0300

    all good

--- OceanFleetButtle/MainWindow.xaml.cs
+++ OceanFleetButtle/MainWindow.xaml.cs
@@ -15,12 +15,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-class PositionException : Exception
-{
-    public PositionException(string pos) : base(pos)
-    {
-    }
-}
 
 namespace OceanFleetButtle
 {
@@ -41,11 +35,12 @@ namespace OceanFleetButtle
             }
             StartPlaying();
             MessageBox.Show("Вам необходимо разместить корабли" +
-                "(см. в левом нижнем углу). Щелчок по кораблю меняет ориентацию.",
-                "Игра Морской бой");
+                            "(см. в левом нижнем углу). Щелчок по кораблю меняет ориентацию.",
+                            "Игра Морской бой");
         }
 
         List<Rectangle> shipList = new List<Rectangle>();
+        List<Tuple<int, int>> computerAttacker = new List<Tuple<int, int>>();
         bool dragging = false;
         int[,] shipArray = new int[10, 10];
         int[,] shipComputerArray = new int[10, 10];
@@ -53,55 +48,80 @@ namespace OceanFleetButtle
         bool playing = false;
         int allFields = 35;
         int myAllFields = 35;
-
+        int position = 0;
         public void Restruct(object sender, RoutedEventArgs e)
         {
-            if (!playing)
+            if (playing)
             {
-                allShips = 0;
-                foreach (var ship in shipList)
-                {
-                    ship.Fill = new SolidColorBrush(Colors.Red);
-                }
-                for (int i = 0; i < 10; i++) 
-                for (int j = 0; j < 10; j++) shipArray[i, j] = 0;
+                return;
             }
+
+            allShips = 0;
+            foreach (var ship in shipList)
+            {
+                ship.Fill = new SolidColorBrush(Colors.Red);
+            }
+
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    shipArray[i, j] = 0;
         }
         public void ComputerAdding(int i)
         {
-            ComputerAddingHorisontal(i);
-            ComputerAddingVertical(i);
-            var RestructButton = new Button();
+            ComputerAddingHorisontal(i, true);
+            ComputerAddingHorisontal(i, false);
+            Button RestructButton = new Button();
+            position = canvy.Children.Count;
+            canvy.Children.Add(RestructButton);
             RestructButton.Content = "Перестроить";
             RestructButton.Width = 100;
             RestructButton.Height = 30;
-            canvy.Children.Add(RestructButton);
             Canvas.SetRight(RestructButton, 5);
             Canvas.SetBottom(RestructButton, 5);
             RestructButton.Click += new RoutedEventHandler(Restruct);
         }
 
-        public void ComputerAddingHorisontal(int i)
+        public void ComputerAddingHorisontal(int i, bool pos)
         {
+            if (i != 0)
+            {
+                InitLine(i, pos);
+            }
             var line = new Line();
-            line.X1 = 25;
-            line.Y1 = 25 + i * 25;
-            line.X2 = 275;
-            line.Y2 = 25 + i * 25;
+            if (pos)
+            {
+                line.X1 = 25;
+                line.Y1 = 25 + i * 25;
+                line.X2 = 275;
+                line.Y2 = 25 + i * 25;
+            }
+            else
+            {
+                line.X1 = 25 + i * 25;
+                line.Y1 = 25;
+                line.X2 = 25 + i * 25;
+                line.Y2 = 275;
+            }
             line.Stroke = new SolidColorBrush(Colors.Blue);
             canvy.Children.Add(line);
-
         }
 
-        public void ComputerAddingVertical(int i)
+        private void InitLine(int i, bool pos)
         {
-            var line2 = new Line();
-            line2.X1 = 25 + i * 25;
-            line2.Y1 = 25;
-            line2.X2 = 25 + i * 25;
-            line2.Y2 = 275;
-            line2.Stroke = new SolidColorBrush(Colors.Blue);
-            canvy.Children.Add(line2);
+            var label = new Label();
+            if (pos) label.Content = (i.ToString());
+            else label.Content = (((char)(i - 1 + 'A')).ToString());
+            canvy.Children.Add(label);
+            if (pos)
+            {
+                Canvas.SetLeft(label, 2);
+                Canvas.SetTop(label, 25 * (i));
+            }
+            else
+            {
+                Canvas.SetLeft(label, 25 * (i));
+                Canvas.SetTop(label, 2);
+            }
         }
 
         public void ComputerAddingButton(int x, int y)
@@ -118,31 +138,52 @@ namespace OceanFleetButtle
 
         public void MyAdding(int i)
         {
-            MyAddingHorisontal(i);
-            MyAddingVertical(i);
+            MyAddingHorisontal(i, true);
+            MyAddingHorisontal(i, false);
         }
-
-        public void MyAddingHorisontal(int i)
+        public void MyAddingHorisontal(int i, bool pos)
         {
+            if (i != 0)
+            {
+                InitMyLine(i, pos);
+            }
             var line3 = new Line();
-            line3.X1 = 300;
-            line3.Y1 = 25 + i * 25;
-            line3.X2 = 550;
-            line3.Y2 = 25 + i * 25;
+            if (pos)
+            {
+                line3.X1 = 300;
+                line3.Y1 = 25 + i * 25;
+                line3.X2 = 550;
+                line3.Y2 = 25 + i * 25;
+            }
+            else
+            {
+                line3.X1 = 300 + i * 25;
+                line3.Y1 = 25;
+                line3.X2 = 300 + i * 25;
+                line3.Y2 = 275;
+            }
             line3.Stroke = new SolidColorBrush(Colors.Red);
             canvy.Children.Add(line3);
         }
 
-        public void MyAddingVertical(int i)
+        private void InitMyLine(int i, bool pos)
         {
-            var line4 = new Line();
-            line4.X1 = 300 + i * 25;
-            line4.Y1 = 25;
-            line4.X2 = 300 + i * 25;
-            line4.Y2 = 275;
-            line4.Stroke = new SolidColorBrush(Colors.Red);
-            canvy.Children.Add(line4);
+            var label = new Label();
+            if (pos) label.Content = (((char)(i + 'A' - 1)).ToString());
+            else label.Content = (i.ToString());
+            canvy.Children.Add(label);
+            if (pos)
+            {
+                Canvas.SetLeft(label, 275 + 25 * (i));
+                Canvas.SetTop(label, 2);
+            }
+            else
+            {
+                Canvas.SetLeft(label, 277);
+                Canvas.SetTop(label, 25 * (i));
+            }
         }
+
         public void StartPlaying()
         {
             for (int i = 1; i <= 5; i++)
@@ -173,7 +214,7 @@ namespace OceanFleetButtle
             else
             {
                 curField.Visibility = Visibility.Hidden;
-                MessageBox.Show("Мимо!");
+                //MessageBox.Show("Мимо!");
                 ComputerStickBack();
             }
         }
@@ -183,39 +224,47 @@ namespace OceanFleetButtle
             shipComputerArray[IntX, IntY] = 0;
             curField.Background = new SolidColorBrush(Colors.Yellow);
             allFields--;
-            MessageBox.Show("Ранил!");
+            //MessageBox.Show("Ранил!");
             if (allFields == 0)
             {
                 FinishPlaying();
             }
         }
 
+        public void AttackGeneration()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    computerAttacker.Add(Tuple.Create(i, j));
+                }
+            }
+        }
+
         public void ComputerStickBack()
         {
             var rand = new Random();
-            var x = rand.Next(10);
-            var y = rand.Next(10);
+            var el = rand.Next(computerAttacker.Count - 1);
+            var x = computerAttacker[el].Item1;
+            var y = computerAttacker[el].Item2;
+            computerAttacker.RemoveAt(el);
             if (shipArray[x, y] == 1)
             {
                 DamagingMe(x, y);
             }
             else
             {
-                MessageBox.Show("Противник не попал");
+                //MessageBox.Show("Противник не попал");
+                ShowDamage(x, y, false);
             }
         }
 
         private void DamagingMe(int x, int y)
         {
             shipArray[x, y] = 0;
-            var damage = new Rectangle();
-            damage.Width = 25;
-            damage.Height = 25;
-            damage.Fill = new SolidColorBrush(Colors.Yellow);
-            canvy.Children.Add(damage);
-            Canvas.SetLeft(damage, 300 + 25 * x);
-            Canvas.SetTop(damage, 25 + 25 * y);
-            MessageBox.Show("Противник попал");
+            ShowDamage(x, y, true);
+            //MessageBox.Show("Противник попал");
             myAllFields--;
             if (myAllFields == 0)
             {
@@ -225,6 +274,18 @@ namespace OceanFleetButtle
             {
                 ComputerStickBack();
             }
+        }
+
+        private void ShowDamage(int x, int y, bool color)
+        {
+            var damage = new Rectangle();
+            damage.Width = 25;
+            damage.Height = 25;
+            if (color) damage.Fill = new SolidColorBrush(Colors.Yellow);
+            else damage.Fill = new SolidColorBrush(Colors.AliceBlue);
+            canvy.Children.Add(damage);
+            Canvas.SetLeft(damage, 300 + 25 * x);
+            Canvas.SetTop(damage, 25 + 25 * y);
         }
 
         public void FinishPlaying()
@@ -259,7 +320,6 @@ namespace OceanFleetButtle
 
         void MyAttack(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show(((Button)e.Source).Background.ToString());
             if (playing)
             {
                 var curField = (Button)e.Source;
@@ -272,10 +332,11 @@ namespace OceanFleetButtle
                 }
             }
         }
+        string greenColor = "#FFFF0000";
         public void EvMouseDown(object sender, RoutedEventArgs e)
         {
             var curShip = (Rectangle)e.Source;
-            if (curShip.Fill.ToString() == "#FFFF0000")
+            if (curShip.Fill.ToString() == greenColor)
             {
                 dragging = true;
                 Mouse.Capture(curShip);
@@ -297,12 +358,12 @@ namespace OceanFleetButtle
         {
             foreach (var ship in shipList)
             {
-                if (ship.Fill.ToString() == "#FFFF0000")
+                if (ship.Fill.ToString() == greenColor)
                 {
                     var heigth = ship.Height;
                     ship.Height = ship.Width;
                     ship.Width = heigth;
-                }   
+                }
             }
         }
 
@@ -330,10 +391,11 @@ namespace OceanFleetButtle
             int intY = (Convert.ToInt32(y) + 13) / 25 - 1;
             return Tuple.Create(intX, intY);
         }
-        public void FillShipArray(Rectangle ship, int x, int y)
+        public bool FillShipArray(Rectangle ship, int x, int y)
         {
             var width = Convert.ToInt16(ship.Width) / 25;
             var height = Convert.ToInt16(ship.Height) / 25;
+            var isValid = true;
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
@@ -342,12 +404,13 @@ namespace OceanFleetButtle
                     {
                         for (int ky = -1; ky < 2; ky++)
                         {
-                            Filling(x, y, i, j, kx, ky);
+                            if (isValid) isValid = Filling(x, y, i, j, kx, ky);
                         }
                     }
                 }
             }
-            FillMyArray(x, y, width, height);
+            if (isValid) FillMyArray(x, y, width, height);
+            return isValid;
         }
 
         private void FillMyArray(int x, int y, int width, int height)
@@ -362,15 +425,16 @@ namespace OceanFleetButtle
             }
         }
 
-        private void Filling(int x, int y, int i, int j, int kx, int ky)
+        private bool Filling(int x, int y, int i, int j, int kx, int ky)
         {
             try
             {
-                if (shipArray[i + x + kx, j + y + ky] == 1)
-                    throw new PositionException("Uncorrect position");
+                if (shipArray[i + x + kx, j + y + ky] == 1) return false;
+                return true;
             }
             catch (IndexOutOfRangeException)
             {
+                return true;
             }
         }
 
@@ -395,6 +459,8 @@ namespace OceanFleetButtle
                 playing = true;
                 GenerateComputerField();
                 MessageBox.Show("А теперь давайте поиграем!");
+                //canvy.Children[position].Visibility = Visibility.Hidden;
+                AttackGeneration();
             }
         }
 
@@ -406,20 +472,17 @@ namespace OceanFleetButtle
             allShips--;
         }
 
-        private void FillRectangle(Rectangle uncorrect, int intX, int intY)
+        private void FillRectangle(Rectangle incorrect, int intX, int intY)
         {
-            Canvas.SetLeft(uncorrect, intX * 25 + 300);
-            Canvas.SetTop(uncorrect, intY * 25 + 25);
-            try
+            Canvas.SetLeft(incorrect, intX * 25 + 300);
+            Canvas.SetTop(incorrect, intY * 25 + 25);
+            var isValid = FillShipArray(incorrect, intX, intY);
+            incorrect.Fill = new SolidColorBrush(Colors.Green);
+            if (!isValid)
             {
-                FillShipArray(uncorrect, intX, intY);
-                uncorrect.Fill = new SolidColorBrush(Colors.Green);
-
-            }
-            catch (PositionException)
-            {
-                Canvas.SetTop(uncorrect, 325);
-                Canvas.SetLeft(uncorrect, 25);
+                incorrect.Fill = new SolidColorBrush(Colors.Red);
+                Canvas.SetTop(incorrect, 325);
+                Canvas.SetLeft(incorrect, 25);
                 allShips--;
             }
         }
@@ -427,8 +490,8 @@ namespace OceanFleetButtle
         private static bool DoingCondicion(Rectangle uncorrect, int intX, int intY)
         {
             return (intX < 0) || (intY < 0) ||
-                            (intX > 10 - Convert.ToInt64(uncorrect.Width) / 25)
-                            || (intY > 10 - uncorrect.Height / 25);
+                    (intX > 10 - Convert.ToInt64(uncorrect.Width) / 25) ||
+                    (intY > 10 - uncorrect.Height / 25);
         }
 
         public void GenerateComputerField()
